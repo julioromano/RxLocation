dfa5c879b3b89f86f8faa0c62c11d6312b3e6f9a
Marco Romano <marco@duckma.com>
2016-07-30 15:13:24 +0200

    First test.

---
+++ rxlocationtest/src/androidTest/java/net/kjulio/rxlocationtest/RxLocationTest.java
@@ -0,0 +1,64 @@
+package net.kjulio.rxlocationtest;
+
+import android.content.Context;
+import android.location.Location;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import net.kjulio.rxlocation.RxLocation;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import rx.android.schedulers.AndroidSchedulers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class RxLocationTest {
+
+//    @Rule
+//    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    private Context context; // Context of the app under test.
+    private RxLocation rxLocation; // Class under test.
+
+    @Before
+    public void setUp() {
+        context = InstrumentationRegistry.getTargetContext();
+        rxLocation = RxLocation.newInstance(context);
+    }
+
+    @Test
+    public void appPackageName() {
+        assertEquals("net.kjulio.rxlocationtest", context.getPackageName());
+    }
+
+    @Test
+    public void testBasicLocation1() {
+        Location location = rxLocation.locationSingle().observeOn(AndroidSchedulers.mainThread()).toBlocking().value();
+        assertNotNull(location);
+    }
+
+    @Test
+    public void testBasicLocation2() {
+        Location location = rxLocation.locationObservable().observeOn(AndroidSchedulers.mainThread()).toBlocking().first();
+        assertNotNull(location);
+    }
+
+    @Test
+    public void testBasicLocation3() {
+        Location location = rxLocation.lastLocation();
+        assertNotNull(location);
+    }
+
+    @After
+    public void tearDown() {
+        context = null;
+        rxLocation = null;
+    }
+
+}
