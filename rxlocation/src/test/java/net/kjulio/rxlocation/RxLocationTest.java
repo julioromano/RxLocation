4f3a03c3e79560fb0e6bf644f60aeeee9a80b843
Marco Romano <marcojulioromano@gmail.com>
2016-10-13 16:35:19 +0200

    Partially working Robolectric tests. The non working ones have been commented out.

--- rxlocation/src/test/java/net/kjulio/rxlocation/RxLocationTest.java
+++ rxlocation/src/test/java/net/kjulio/rxlocation/RxLocationTest.java
@@ -1,75 +1,126 @@
 package net.kjulio.rxlocation;
 
+import android.Manifest;
 import android.content.Context;
 import android.location.Location;
 import android.os.HandlerThread;
 
-import org.junit.Assert;
+import com.google.android.gms.location.LocationRequest;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.internal.ShadowExtractor;
-import org.robolectric.shadows.ShadowLooper;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowApplication;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.observers.TestSubscriber;
 
-import static java.lang.Thread.sleep;
-
 @RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class RxLocationTest {
 
     private Context context;
+    private ShadowApplication application;
+    private LocationRequest locationRequest;
     private TestSubscriber<Location> testSubscriber;
-    private HandlerThread handlerThread;
 
     @Before
     public void setUp() {
         context = RuntimeEnvironment.application.getApplicationContext();
+        application = Shadows.shadowOf(RuntimeEnvironment.application);
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
         testSubscriber = TestSubscriber.create();
-        handlerThread = new HandlerThread("testHandlerThread");
-        handlerThread.start();
+
+        application.grantPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION);
     }
 
+    //    @Test
+//    public void lastLocationConnectionFailed() {
+//        // Failed run of lastLocation() observable.
+//        // GoogleApiClientFactory.setMockConnectionFailure(true);
+//        MockRxLocation.lastLocation(context)
+//                .subscribeOn(AndroidSchedulers.mainThread())
+//                .toBlocking()
+//                .subscribe(testSubscriber);
+//        ShadowLooper.runUiThreadTasks();
+//        Assert.assertTrue(testSubscriber.getOnErrorEvents().size() >= 1);
+//    }
+
+//    @Test
+//    public void testLastLocation() {
+//        MockRxLocation.lastLocation(context)
+//                .subscribe(testSubscriber);
+//        testSubscriber.awaitTerminalEvent();
+//        testSubscriber.assertValueCount(1);
+//        testSubscriber.assertCompleted();
+//    }
+
     @Test
-    public void connectionSuccessful() {
-        // Successful run of lastLocation() observable.
+    public void testLastLocationUiThread() {
         MockRxLocation.lastLocation(context)
-                .subscribeOn(AndroidSchedulers.from(handlerThread.getLooper()))
-                .toBlocking()
+                .subscribeOn(AndroidSchedulers.mainThread())
                 .subscribe(testSubscriber);
 //        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
-//        ((ShadowLooper) ShadowExtractor.extract(handlerThread.getLooper())).runToEndOfTasks();
-        try {
-            sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        testSubscriber.awaitTerminalEvent();
         testSubscriber.assertValueCount(1);
         testSubscriber.assertCompleted();
     }
 
 //    @Test
-//    public void connectionFailed() {
-//        // Failed run of lastLocation() observable.
-//        // GoogleApiClientFactory.setMockConnectionFailure(true);
+//    public void testLastLocationHandlerThread() {
+//        HandlerThread handlerThread = new HandlerThread("MockHandlerThread");
+//        handlerThread.start();
 //        MockRxLocation.lastLocation(context)
-//                .subscribeOn(AndroidSchedulers.mainThread())
-////                .toBlocking()
+//                .subscribeOn(AndroidSchedulers.from(handlerThread.getLooper()))
 //                .subscribe(testSubscriber);
-//        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
-//        Assert.assertTrue(testSubscriber.getOnErrorEvents().size() >= 1);
+////        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
+//        testSubscriber.awaitTerminalEvent();
+//        testSubscriber.assertValueCount(1);
+//        testSubscriber.assertCompleted();
+//    }
+
+//    @Test
+//    public void testLastLocationNewThread() {
+//        MockRxLocation.lastLocation(context)
+//                .subscribeOn(Schedulers.newThread())
+//                .subscribe(testSubscriber);
+//        testSubscriber.awaitTerminalEvent();
+//        testSubscriber.assertValueCount(1);
+//        testSubscriber.assertCompleted();
 //    }
 
 //    @Test
 //    public void testLocationUpdates() {
-//        // Successful run of locationUpdates() observable.
-//        LocationRequest defaultLocationRequest = LocationRequest.create()
-//                .setPriority(LocationRequest.PRIORITY_NO_POWER);
-//        MockRxLocation.locationUpdates(context, defaultLocationRequest)
-//                .first().toBlocking().subscribe(testSubscriber);
+//        RxLocation.locationUpdates(context, locationRequest)
+//                .first()
+//                .subscribe(testSubscriber);
+//        testSubscriber.awaitTerminalEvent();
+//        testSubscriber.assertValueCount(1);
+//    }
+
+//    @Test
+//    public void testLocationUpdatesUiThread() {
+//        RxLocation.locationUpdates(context, locationRequest)
+//                .subscribeOn(AndroidSchedulers.mainThread())
+//                .first()
+//                .subscribe(testSubscriber);
+//        testSubscriber.awaitTerminalEvent();
+//        testSubscriber.assertValueCount(1);
+//    }
+
+//    @Test
+//    public void testLocationUpdatesNewThread() {
+//        RxLocation.locationUpdates(context, locationRequest)
+//                .subscribeOn(Schedulers.newThread())
+//                .first()
+//                .subscribe(testSubscriber);
+//        testSubscriber.awaitTerminalEvent();
 //        testSubscriber.assertValueCount(1);
 //    }
 }
