3f04bb4bc03f54c2098f3071b4ecf54fd89f8a83
Marco Romano <marcojulioromano@gmail.com>
2016-10-13 15:00:35 +0200

    Allow use of library also in non static form (i.e. allow instantiation of RxLocation stateless class and expose non static methods that wrap the static ones.)

--- rxlocation/src/main/java/net/kjulio/rxlocation/RxLocation.java
+++ rxlocation/src/main/java/net/kjulio/rxlocation/RxLocation.java
@@ -11,15 +11,56 @@ import rx.Observable;
  * RxLocation.
  *
  * An easy to use Android library to get location data in an RxJava way.
- *
  * https://github.com/julioromano/RxLocation
+ *
+ *
+ * All public methods of the library have a static and non-static version.
+ *
+ * The static methods can be invoked straight away:
+ * Observable<Location> o = RxLocation.lastLocation(context);
+ *
+ * The non-static methods require an RxLocation instance instead:
+ * RxLocation rxLocation = new RxLocation(context);
+ * Observable<Location> o = rxLocation.lastLocation();
+ *
+ * The library is stateless: there is no difference in using the static vs non-static version.
  */
 public class RxLocation {
 
-    private RxLocation() {}
+    private final Context context;
+
+    public RxLocation(Context context) {
+        this.context = context;
+    }
 
     /**
-     * Clients subscribe to get location updates via this method.
+     * Yields periodical location updates.
+     *
+     * This observable will never call onComplete() thus manual unsubscribe() is necessary.
+     *
+     * When using setExpirationDuration() or setNumUpdates() or setExpirationTime() the observable
+     * will not terminate automatically and will just stop emitting new items without releasing any
+     * resources.
+     *
+     * @return an Observable that returns Location items.
+     */
+    public Observable<Location> locationUpdates(LocationRequest locationRequest) {
+        return RxLocation.locationUpdates(context, locationRequest);
+    }
+
+    /**
+     * Yields the last location available to the system.
+     *
+     * This observable will emit only one element and then call onComplete.
+     *
+     * @return an Observable that returns one Location item.
+     */
+    public Observable<Location> lastLocation() {
+        return RxLocation.lastLocation(context);
+    }
+
+    /**
+     * Yields periodical location updates.
      *
      * This observable will never call onComplete() thus manual unsubscribe() is necessary.
      *
@@ -35,6 +76,13 @@ public class RxLocation {
                 new FusedLocationProviderFactoryImpl(), locationRequest);
     }
 
+    /**
+     * Yields the last location available to the system.
+     *
+     * This observable will emit only one element and then call onComplete.
+     *
+     * @return an Observable that returns one Location item.
+     */
     public static Observable<Location> lastLocation(Context context) {
         return LastLocationHelper.observable(context, new GoogleApiClientFactoryImpl(),
                 new FusedLocationProviderFactoryImpl());
